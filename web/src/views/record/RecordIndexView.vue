1cbe6509a3029a918578af6133691253635287ff
李行健 <2049946855@qq.com>
2023-04-30 11:17:17 +0800

    前端实现对战记录、用户排行榜的分页，后端实现用户排行榜的分页(8完结)

--- web/src/views/record/RecordIndexView.vue
+++ web/src/views/record/RecordIndexView.vue
@@ -32,6 +32,29 @@
                 </tr>
             </tbody>
         </table>
+        <nav aria-label="...">
+            <ul class="pagination" style="float: right;">
+                <li class="page-item">
+                    <a class="page-link" href="#" @click="click_page(1)">
+                        <span aria-hidden="true">&laquo;</span>
+                    </a>
+                </li>
+                <li class="page-item" @click="click_page(-2)">
+                    <a class="page-link" href="#">前一页</a>
+                </li>
+                <li :class="'page-item ' + page.is_active" v-for="page in pages" :key="page.number" @click="click_page(page.number)">
+                    <a class="page-link" href="#">{{ page.number }}</a>
+                </li>
+                <li class="page-item" @click="click_page(-1)">
+                    <a class="page-link" href="#">后一页</a>
+                </li>
+                <li class="page-item" @click="click_page(0)">
+                    <a class="page-link" href="#">
+                        <span aria-hidden="true">&raquo;</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
     </ContentField>
 </template>
 
@@ -52,8 +75,32 @@ export default {
         let records = ref([]);
         let current_page = 1;
         let total_records = 0;
+        let pages = ref([]);
 
-        console.log(total_records);
+        const click_page = page => {
+            let max_pages = parseInt(Math.ceil(total_records / 10));
+            if (page === -2) page = current_page - 1;
+            else if (page === -1) page = current_page + 1;
+            else if (page === 0) page = max_pages;
+
+            if(page >= 1 && page <= max_pages) {
+                pull_page(page);
+            }
+        }
+
+        const update_pages = () => {
+            let max_pages = parseInt(Math.ceil(total_records / 10));
+            let new_pages = [];
+            for(let i = current_page - 2; i <= current_page + 2; i++) {
+                if(i >= 1 && i <= max_pages) {
+                    new_pages.push({
+                        number: i,
+                        is_active: i === current_page ? "active" : "",
+                    });
+                }
+            }
+            pages.value = new_pages;
+        }
 
         const pull_page = page => {//查询某一个页面的内容
             current_page = page;
@@ -69,6 +116,7 @@ export default {
                 success(resp) {
                     records.value = resp.records;
                     total_records = resp.records_count;
+                    update_pages();
                 },
                 error(resp) {
                     console.log(resp);
@@ -123,7 +171,9 @@ export default {
 
         return {
             records,
-            open_record_content
+            open_record_content,
+            pages,
+            click_page,
         }
     }
 }
